a555384f051aae6dd0be38bc71823659d27fb264
Shailesh <Shailesh.pandit3010@gmail.com>
2023-12-13 15:44:15 +0530

    Eclipse changes

--- GitHubDemo2/src/main/java/com/git/cases/Login_page.java
+++ GitHubDemo2/src/main/java/com/git/cases/Login_page.java
@@ -5,6 +5,8 @@ public class Login_page {
 	public void testCase1() {
 		System.out.println("this is login method");
 		System.out.println("this is login2 method");
+		
+		System.out.println("Nikhil is coming");
 	}
 
 }
